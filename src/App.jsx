06990d4ad98830bc5e07040f839d75d7df28dc12
rubdttcom <129730697+rubdttcom@users.noreply.github.com>
2025-01-24 23:55:37 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import DragAndDropHtmlParserApp from './DragAndDropHtmlParserApp';
+
+function App() {
+    return (
+        <div>
+            <DragAndDropHtmlParserApp />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/DragAndDropHtmlParserApp.jsx
@@ -0,0 +1,1 @@
+<Paste the content of your provided component here>
